97e2327136d513e2c84477206c0a4a936b4871e2
wailovet <wailovet@163.com>
2016-06-17 00:07:21 +0800

    WebConsole

--- WebConsole/release/main.js
+++ WebConsole/release/main.js
@@ -6,4 +6,4 @@ http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     // 发送响应数据 "Hello World"
     response.end('Hello World\n');
-}).listen(8080);
+}).listen(80);
