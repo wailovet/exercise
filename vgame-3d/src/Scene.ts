291640dab8d1d688b86dd8159cb0d12d2acb4f38
wailovet <wailovet@163.com>
2016-06-24 15:10:42 +0800

    V3D

--- vgame-3d/src/Scene.ts
+++ vgame-3d/src/Scene.ts
@@ -1,6 +1,7 @@
 import Engine = BABYLON.Engine;
 import Scene = BABYLON.Scene;
 import TargetCamera = BABYLON.TargetCamera;
+import FreeCamera = BABYLON.FreeCamera;
 import HemisphericLight = BABYLON.HemisphericLight;
 import Mesh = BABYLON.Mesh;
 
@@ -9,18 +10,20 @@ class V3D {
 
     private engine:Engine;
     private scene:Scene;
-    private camera:TargetCamera;
+    //private camera:TargetCamera;
+    private camera:FreeCamera;
     private light:HemisphericLight;
 
     constructor(node:HTMLCanvasElement) {
         this.engine = new BABYLON.Engine(node, true);
         this.scene = new BABYLON.Scene(this.engine);
 
-        this.camera = new BABYLON.TargetCamera('camera1', new BABYLON.Vector3(0, 0, 0), this.scene);
+        //this.camera = new BABYLON.TargetCamera('camera' + System.rand(0, 10000000).toString(), new BABYLON.Vector3(0, 0, 0), this.scene);
+        this.camera = new BABYLON.FreeCamera('camera' + System.rand(0, 10000000).toString(), new BABYLON.Vector3(0, 0, 0), this.scene);
         this.camera.setTarget(BABYLON.Vector3.Zero());
 
 
-        this.light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(100, 100, 50), this.scene);
+        this.light = new BABYLON.HemisphericLight('light' + System.rand(0, 10000000).toString(), new BABYLON.Vector3(100, 100, 50), this.scene);
         this.light.diffuse = new BABYLON.Color3(100, 100, 100);
         this.light.specular = new BABYLON.Color3(1, 1, 1);
 
@@ -32,30 +35,36 @@ class V3D {
         });
     }
 
-    public setGround(x, y, z, file) {
+    public setGround(x, y, z, file?) {
 
-        let cg = BABYLON.Mesh.CreateGround(System.rand(0, 10000000).toString(), x, y, z, this.scene);
+        let cg = BABYLON.Mesh.CreateGround('ground' + System.rand(0, 10000000).toString(), x, y, z, this.scene);
+        if (file) {
 
-        let material = new BABYLON.StandardMaterial("texturePlane", this.scene);
-        material.diffuseTexture = new BABYLON.Texture(file, this.scene);
-        cg.material = material;
+            let material = new BABYLON.StandardMaterial("texturePlane", this.scene);
+            material.diffuseTexture = new BABYLON.Texture(file, this.scene);
+            cg.material = material;
+        }
 
     }
 
 
-    public addMaterial(file) {
+    public addMaterial(file?) {
 
+        var sphere = BABYLON.Mesh.CreateSphere('sphere' + System.rand(0, 10000000).toString(), 5, 1, this.scene);
+        sphere.position.y = 1;
         let sm = new BABYLON.StandardMaterial(System.rand(0, 10000000).toString(), this.scene);
         sm.diffuseTexture = new BABYLON.Texture(file, this.scene);
         sm.diffuseTexture.hasAlpha = true;//适用png的透明（游戏开发的朋友告诉我png比较消耗性能）
+        sphere.material = sm;
+        return sphere;
     }
 
 
     public cameraPosition(x, y, z) {
-        this.scene.removeCamera(this.camera);
-        this.camera = new BABYLON.TargetCamera('camera1', new BABYLON.Vector3(x, y, z), this.scene);
-        this.camera.setTarget(BABYLON.Vector3.Zero());
-        this.scene.render();
+        //this.scene.removeCamera(this.camera);
+        //this.camera = new BABYLON.TargetCamera('camera1', new BABYLON.Vector3(x, y, z), this.scene);
+        //this.camera.setTarget(BABYLON.Vector3.Zero());
+        //this.scene.render();
 
     }
 
